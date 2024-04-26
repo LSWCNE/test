b05d9d84312fe5381cda1751403522e662e00910
LSWCNE <167836435+LSWCNE@users.noreply.github.com>
2024-04-26 13:47:48 +0900

    Add files via upload

---
+++ 특강때 할거/1.py
@@ -0,0 +1,14 @@
+# 1에서 20까지의 정수를 출력하시오.
+# for num in range(1, 21):
+#     print(num, end="")
+# num = []
+# while len(num) < 21:
+#     for i in range(1, 21):
+#         num.append(i)
+#     break
+# print(num)
+
+count = 1
+while count < 20:
+    count += 1
+    print(count)
